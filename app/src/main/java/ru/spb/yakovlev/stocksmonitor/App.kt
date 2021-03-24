f47426aeabdeb8cac9a609990c8751c45fe81a74
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-03-24 09:35:01 +0300

    Initial commit

---
+++ app/src/main/java/ru/spb/yakovlev/stocksmonitor/App.kt
@@ -0,0 +1,26 @@
+package ru.spb.yakovlev.stocksmonitor
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+        initLogger()
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    companion object {
+        internal lateinit var INSTANCE: App
+            private set
+    }
+}
