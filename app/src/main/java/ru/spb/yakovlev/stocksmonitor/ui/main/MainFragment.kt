aebed3b3fa90a7578b871e5db7434f4209b529fe
Aleksey <aleksey@yakovlev.spb.ru>
2021-03-28 00:41:38 +0300

    Icons added

--- app/src/main/java/ru/spb/yakovlev/stocksmonitor/ui/main/MainFragment.kt
+++ app/src/main/java/ru/spb/yakovlev/stocksmonitor/ui/main/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -31,8 +32,13 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     }
 
     private fun setupViews() {
-
         setupViewPager()
+
+    }
+
+    private fun setupSearch(){
+
+
 
     }
 
