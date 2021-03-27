a6b1bc0e0773cd9b2be449fc99e3c34364fd3e35
Aleksey <aleksey@yakovlev.spb.ru>
2021-03-28 00:44:38 +0300

    Main screen updated

--- app/src/main/java/ru/spb/yakovlev/stocksmonitor/ui/RootActivity.kt
+++ app/src/main/java/ru/spb/yakovlev/stocksmonitor/ui/RootActivity.kt
@@ -1,34 +1,18 @@
 package ru.spb.yakovlev.stocksmonitor.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import by.kirich1409.viewbindingdelegate.viewBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import ru.spb.yakovlev.stocksmonitor.R
-import ru.spb.yakovlev.stocksmonitor.databinding.ActivityRootBinding
 
 @AndroidEntryPoint
 class RootActivity : AppCompatActivity(R.layout.activity_root) {
-    private val vb by viewBinding(ActivityRootBinding::bind, R.id.root_container)
+    lateinit var navController: NavController
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        //val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment)
-//
-//
-//
-////        if (savedInstanceState == null) {
-////            supportFragmentManager.beginTransaction()
-////                    .replace(R.id.container, MainFragment.newInstance())
-////                    .commitNow()
-////        }
-//
-//    }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        navController = findNavController(R.id.nav_host_fragment)
+    }
 }
